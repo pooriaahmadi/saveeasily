fb609746c86c0c2ca5a8efceb88238efa82e33d5
pooriaahmadi <pooriababak444@gmail.com>
2021-08-23 15:31:00 +0430

    config.ts update

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,32 @@
 const testGuild: string = "864457765907988481";
-const clientId: string = "776890560794853416";
-const databaseConfig = {
-	host: "localhost",
-	port: 3306,
-	username: "root",
-	password: "",
-	databaseName: "saveeasily",
+const debug: boolean = false;
+let clientId: string;
+let databaseConfig: {
+	host: string;
+	port: number;
+	username: string;
+	password: string;
+	databaseName: string;
 };
+if (debug) {
+	clientId = "776890560794853416";
+	databaseConfig = {
+		host: "localhost",
+		port: 3306,
+		username: "root",
+		password: "",
+		databaseName: "saveeasily",
+	};
+} else {
+	clientId = "879048523801317386";
+	databaseConfig = {
+		host: "localhost",
+		port: 3306,
+		username: "root",
+		password: "Kindertouch123",
+		databaseName: "saveeasily",
+	};
+}
+
 const footer: string = "SaveEasily!";
 export { testGuild, clientId, databaseConfig, footer };

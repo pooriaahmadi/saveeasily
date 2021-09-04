540c90f5b274275a231725e5fd2a4418e614ee41
pooriaahmadi <pooriababak444@gmail.com>
2021-09-04 23:53:55 +0430

    remote connection

--- src/config.ts
+++ src/config.ts
@@ -20,7 +20,7 @@ if (debug) {
 } else {
 	clientId = "879048523801317386";
 	databaseConfig = {
-		host: "192.168.1.50",
+		host: "79.127.106.158",
 		port: 3306,
 		username: "remote",
 		password: "Kindertouch123",
